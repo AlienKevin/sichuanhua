5c26126758d034c9ba0db077acfcb7577d608f70
Kevin Li <kevinli020508@gmail.com>
2023-05-04 21:03:29 +0800

    Can lookup Cendu pinyin

--- sichuanhua/main.cpp
+++ sichuanhua/main.cpp
@@ -5,6 +5,7 @@
 #include <string>
 #include <numeric>
 #include <unicode/unistr.h>
+#include <regex>
 
 using namespace std;
 
@@ -75,7 +76,7 @@ PrDict readPrDict() {
     return prs;
 }
 
-vector<string> lookupPrDict(PrDict prs, string word) {
+vector<string> lookupPrDict(const PrDict& prs, const string& word) {
     auto pr = prs.find(word);
     if (pr != prs.end()) {
         return pr->second;
@@ -84,25 +85,75 @@ vector<string> lookupPrDict(PrDict prs, string word) {
     }
 }
 
+string applyRegexRules(const string& pinyin, const vector<pair<regex, string>>& rules) {
+    string result = pinyin;
+    for (auto rule : rules) {
+        result = regex_replace(result, rule.first, rule.second);
+    }
+    return result;
+}
+
+PrDict convertToCenduPrDict(PrDict prs) {
+    vector<pair<regex, string>> rules = {
+        // 特字部分
+        {regex("Ddi"), "le"},    // 的字讀音
+        {regex("Dqu4"), "qie4"}, // 去字讀音
+        {regex("Dlu5"), "liu2"},  // 六字讀音
+        // 通入部分
+        {regex("^su5$"), "sv5"}, // 成都特殊设定・速俗v/io两可
+        // 入聲派向部分
+        {regex("5$"), "2"},
+        // 戈歌部分
+        {regex("eo([1234])$"), "o$1"}, // 不分舒聲戈歌
+        {regex("eo5"), "o5"}, // 不分入聲戈歌
+        // 尖團部分
+        {regex("^zyi"), "ji"}, // zyi>ji (挤)
+        {regex("^cyi"), "qi"}, // cyi>qi (七)
+        {regex("^syi"), "xi"}, // syi>xi (西)
+        {regex("^zi([a-z]+)"), "ji$1"}, // zi->ji- (尖)
+        {regex("^ci([a-z]+)"), "qi$1"}, // ci->qi- (雀)
+        {regex("^si([a-z]+)"), "xi$1"}, // si->xi- (仙)
+        {regex("^zv"), "ju"},  // zü>ju, zü->ju- (聚)
+        {regex("^cv"), "qu"},  // cü>qu, cü->qu- (趣)
+        {regex("^sv"), "xu"},  // sü>xu, sü->xu- (须)
+        // 平翹部分
+        {regex("^([zcs])h"), "$1"}, // 無翹舌
+        // h、f部分
+        {regex("^hu([12345])$"), "fu$1"}, // hu>fu
+        // 成都特字
+        {regex("C"), ""},
+    };
+    for (auto& entry : prs) {
+        for (auto& pr : entry.second) {
+//            cout << "before: " << pr << endl;
+            pr = applyRegexRules(pr, rules);
+//            cout << "after: " << pr << endl;
+        }
+    }
+    return prs;
+}
+
 int main(int argc, char** argv)
 {
     // Parse command-line arguments
     if (argc < 2) {
-        cout << "Usage: sch <word>" << endl;
+        cout << "Usage: sch <word1> (<word2> ...)" << endl;
         return 0;
     }
-    string word = argv[1];
+    PrDict prs = convertToCenduPrDict(readPrDict());
     
-    PrDict prs = readPrDict();
-
-    vector<string> pr_results = lookupPrDict(prs, word);
-    
-    if (pr_results.empty()) {
-        cout << word << " is not found." << endl;
-    } else {
-        cout << word << ": " << accumulate(pr_results.begin(), pr_results.end(), std::string(), [&](auto a, auto b) {
-            return a.empty() ? b : a + ", " + b;
-        }) << endl;
+    for (int i = 1; i < argc; i++) {
+        string word = argv[i];
+        
+        vector<string> pr_results = lookupPrDict(prs, word);
+        
+        if (pr_results.empty()) {
+            cout << word << " is not found." << endl;
+        } else {
+            cout << word << ": " << accumulate(pr_results.begin(), pr_results.end(), std::string(), [&](auto a, auto b) {
+                return a.empty() ? b : a + ", " + b;
+            }) << endl;
+        }
     }
     
     return 0;
--- t2s.py
+++ t2s.py
@@ -1,5 +1,10 @@
 from opencc import OpenCC
 cc = OpenCC('t2s')
 with open("shupin.dict.yaml", "r") as input_file, open("shupin.simp.dict.yaml", "w+") as output_file:
+    prev_lines = set()
     for line in input_file:
-        output_file.write(cc.convert(line))
+        curr_line = cc.convert(line)
+        # remove duplicate lines due to multiple traditional mapped to a single simplified
+        if not curr_line in prev_lines:
+            output_file.write(curr_line)
+            prev_lines.add(curr_line)
